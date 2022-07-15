a84f4259d20c96742f25a4de6441a3af9bff27d7
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2022-07-14 23:26:50 -0400

    Update neuroglancer-stitched.py

--- 000108/chunglab/demo/neuroglancer-stitched.py
+++ 000108/chunglab/demo/neuroglancer-stitched.py
@@ -2,6 +2,7 @@ from dandi.dandiapi import DandiAPIClient
 import json
 from urllib.parse import quote, unquote
 import pandas as pd
+import numpy as np
 
 cubehelix_template = """
 #uicontrol float brightness slider(min=0.0, max=100.0, default=%f)
@@ -19,18 +20,115 @@ void main() {
     emitRGB(result);
 }
 """
-colormap = {"LEC": 10, "YO": 40, "NN": 80}
 
-api = DandiAPIClient("https://api.dandiarchive.org/api")
-ds = api.get_dandiset("000108")
+cubehelix2_template = """
+#uicontrol float brightness slider(min=0.0, max=100.0, default=%f)
+void main() {
+    float x = clamp(toNormalized(getDataValue()) * brightness, 0.0, 1.0);
+    float angle = 2.0 * 3.1415926 * (4.0 / 3.0 + x);
+    float amp = x * (1.0 - x) / 2.0;
+    vec3 result;
+    float cosangle = cos(angle);
+    float sinangle = sin(angle);
+    result.g = -0.14861 * cosangle + 1.78277 * sinangle;
+    result.r = -0.29227 * cosangle + -0.90649 * sinangle;
+    result.b = 1.97294 * cosangle;
+    result = clamp(x + amp * result, 0.0, 1.0);
+    emitRGB(result);
+}
+"""
 
-assets = list(ds.get_assets_by_glob(f"*sub-MITU01/*SPIM.ome.zarr"))
-photos = list(ds.get_assets_by_glob(f"*sub-MITU01/*_photo.jpg"))
-df = pd.DataFrame([dict([val.split("-")
-                         for val in asset.path.split("/")[-1].split(".")[0].split("_")
-                         if "-" in val]) for asset in assets])
-samples = sorted(df['sample'].unique(), key=lambda x: int(x.split("R")[0]))
-samples_w_sessions = sorted([val[0] for val in df.groupby(['sample', 'ses'])], key=lambda x: int(x[0].split("R")[0]))
+cubehelix3_template = """
+#uicontrol float brightness slider(min=0.0, max=100.0, default=%f)
+void main() {
+    float x = clamp(toNormalized(getDataValue()) * brightness, 0.0, 1.0);
+    float angle = 2.0 * 3.1415926 * (4.0 / 3.0 + x);
+    float amp = x * (1.0 - x) / 2.0;
+    vec3 result;
+    float cosangle = cos(angle);
+    float sinangle = sin(angle);
+    result.b = -0.14861 * cosangle + 1.78277 * sinangle;
+    result.g = -0.29227 * cosangle + -0.90649 * sinangle;
+    result.r = 1.97294 * cosangle;
+    result = clamp(x + amp * result, 0.0, 1.0);
+    emitRGB(result);
+}
+"""
+
+cubehelix4_template = """
+#uicontrol float brightness slider(min=0.0, max=100.0, default=%f)
+void main() {
+    float x = clamp(toNormalized(getDataValue()) * brightness, 0.0, 1.0);
+    float angle = 2.0 * 3.1415926 * (4.0 / 3.0 + x);
+    float amp = x * (1.0 - x) / 2.0;
+    vec3 result;
+    float cosangle = cos(angle);
+    float sinangle = sin(angle);
+    result.r = -0.14861 * cosangle + 1.78277 * sinangle;
+    result.g = -0.29227 * cosangle + -0.90649 * sinangle;
+    result.b = 1.97294 * cosangle;
+    result = clamp(x + amp * result, 0.0, 1.0);
+    emitRGB(result);
+}
+"""
+
+cubehelix5_template = """
+#uicontrol float brightness slider(min=0.0, max=100.0, default=%f)
+void main() {
+    float x = clamp(toNormalized(getDataValue()) * brightness, 0.0, 1.0);
+    float angle = 2.0 * 3.1415926 * (4.0 / 3.0 + x);
+    float amp = x * (1.0 - x) / 2.0;
+    vec3 result;
+    float cosangle = cos(angle);
+    float sinangle = sin(angle);
+    result.g = -0.14861 * cosangle + 1.78277 * sinangle;
+    result.b = -0.29227 * cosangle + -0.90649 * sinangle;
+    result.r = 1.97294 * cosangle;
+    result = clamp(x + amp * result, 0.0, 1.0);
+    emitRGB(result);
+}
+"""
+
+cubehelix6_template = """
+#uicontrol float brightness slider(min=0.0, max=100.0, default=%f)
+void main() {
+    float x = clamp(toNormalized(getDataValue()) * brightness, 0.0, 1.0);
+    float angle = 2.0 * 3.1415926 * (4.0 / 3.0 + x);
+    float amp = x * (1.0 - x) / 2.0;
+    vec3 result;
+    float cosangle = cos(angle);
+    float sinangle = sin(angle);
+    result.b = -0.14861 * cosangle + 1.78277 * sinangle;
+    result.r = -0.29227 * cosangle + -0.90649 * sinangle;
+    result.g = 1.97294 * cosangle;
+    result = clamp(x + amp * result, 0.0, 1.0);
+    emitRGB(result);
+}
+"""
+
+colormap = {"LEC": cubehelix_template % 20,
+            "YO": cubehelix2_template % 40,
+            "NN": cubehelix3_template % 80,
+            "CR": cubehelix4_template % 50,
+            'calretinin': cubehelix4_template % 50,
+            'NPY': cubehelix5_template % 50,
+            'npy': cubehelix5_template % 50,
+            'IBA1': cubehelix6_template % 50,
+            'SST': cubehelix4_template % 50}
+
+
+def get_data(sub):
+    api = DandiAPIClient("https://api.dandiarchive.org/api")
+    ds = api.get_dandiset("000108")
+
+    assets = list(ds.get_assets_by_glob(f"*{sub}/*SPIM.ome.zarr"))
+    photos = list(ds.get_assets_by_glob(f"*{sub}/*_photo.jpg"))
+    df = pd.DataFrame([dict([val.split("-")
+                             for val in asset.path.split("/")[-1].split(".")[0].split("_")
+                             if "-" in val]) for asset in assets])
+    samples = sorted(df['sample'].unique(), key=lambda x: int(x.split("R")[0]))
+    samples_w_sessions = sorted([val[0] for val in df.groupby(['sample', 'ses'])], key=lambda x: int(x[0].split("R")[0]))
+    return df, ds, samples, photos
 
 
 def get_photo_url(photos, sample):
@@ -38,37 +136,39 @@ def get_photo_url(photos, sample):
     return photo_url[0] if photo_url else None
 
 
-def get_url(ds, sample, ses, stain):
-    zarrs = list(ds.get_assets_by_glob(f"*sub-MITU01/*_ses-{ses}*_sample-{sample}_stain-{stain}_run-1*.ome.zarr"))
+def get_url(ds, subj, sample, stains):
+    layers = []
+    for stain in stains:
+        zarrs = list(ds.get_assets_by_glob(f"*{subj}/*_sample-{sample}_stain-{stain}_run-1*.ome.zarr"))
 
-    #print([a.path for a in zarrs])
-    sources = [f"zarr://{val.get_content_url(regex='s3')}"
-              for val in sorted(zarrs, key=lambda x: int(x.path.split("_chunk-")[1].split("_")[0]))]
+        sources = [f"zarr://{val.get_content_url(regex='s3')}"
+                  for val in sorted(zarrs, key=lambda x: int(x.path.split("_chunk-")[1].split("_")[0]))]
+        # print([val.path for val in sorted(zarrs, key=lambda x: int(x.path.split("_chunk-")[1].split("_")[0]))])
+        if len(zarrs):
+            val = zarrs[0]
+            layer = dict(
+                source=sources,
+                type="image",
+                shader=colormap[stain],
+                name=val.path.split("_sample-")[1].split("_")[0] + f'-{stain}' + "-" + f'{len(zarrs)}',
+                tab='rendering',
+            )
+            layers.append(layer)
 
-    url = "#"
-    if len(zarrs):
-        val = zarrs[0]
-        layer = dict(
-            source=sources,
-            type="image",
-            shader=cubehelix_template % (colormap[stain] if stain in colormap else 40),
-            name=val.path.split("_sample-")[1].split("_")[0] + "-" + f'{len(zarrs)}',
-            tab='rendering',
-        )
-
-        ng_url = "https://neuroglancer-demo.appspot.com/"
-        ng_str = json.dumps(dict(dimensions={"t":[1,"s"],
-                                             "z":[0.000002285,"m"],
-                                             "y":[0.0000032309999999999996,"m"],
-                                             "x":[0.000002285,"m"]},
-                                 displayDimensions=["z","y","x"],
-                                 crossSectionScale=50,
-                                 projectionScale=500000,
-                                 layers=[layer],
-                                 showDefaultAnnotations=False,
-                                 layout="yz"))
-        url = f"{ng_url}#!%s" % quote(ng_str)
-    return url, len(zarrs)
+    ng_url = "https://neuroglancer-demo.appspot.com/"
+    ng_str = json.dumps(dict(dimensions={"t":[1,"s"],
+                                         "z":[0.000002285,"m"],
+                                         "y":[0.0000032309999999999996,"m"],
+                                         "x":[0.000002285,"m"]},
+                             displayDimensions=["z","y","x"],
+                             crossSectionScale=50,
+                             projectionScale=500000,
+                             layers=layers,
+                             showDefaultAnnotations=False,
+                             layerListPanel={'visible': True},
+                             layout="yz"))
+    url = f"{ng_url}#!%s" % quote(ng_str)
+    return url
 
 
 if __name__ == "__main__":
@@ -106,20 +206,37 @@ if __name__ == "__main__":
 </script>
 <br/>\n
 """
-    for sample, ses in samples_w_sessions:
-        stains = df[df['sample'] == sample].stain.unique()
-        row = f"{sample}: "
-        for stain in stains:
-            url, nchunks = get_url(ds, sample, ses, stain)
-            row += f'<a href="{url}" target="_blank">{stain}+{nchunks}</a> '
-        print(sample, ses, stain, nchunks, len(url))
-        row += f' - <a href="https://dandiarchive.org/dandiset/000108/draft/files?location=sub-MITU01%2Fses-{ses}%2Fmicr%2F">{ses}</a>'
+    sub = 'sub-MITU01'
+    # sub = 'sub-MITU01h3'
+    df, ds, samples, photos = get_data(sub)
+    noprocess = []
+    for sample in samples:
+        stains = []
+        for group in df[df['sample'] == sample].groupby('stain'):
+            if not any((group[1].chunk.value_counts().values > 1).tolist()):
+                stains.append(group[0])
+        if len(stains) == 0:
+            noprocess.append(sample)
+            continue
+        stain_count = df[df['sample'] == sample].groupby('stain').chunk.count()
+        url = get_url(ds, sub, sample, stains)
+        sample_val = f'{int(sample):03d}' if 'R' not in sample else sample
+        row = f'<a href="{url}" target="_blank">Sample: {sample_val}</a> Stains: {list(stain_count.items())}'
+        print(sample, list(stain_count.items()), len(url))
+        for ses in df[df['sample'] == sample].ses.unique():
+            row += f' Session: <a href="https://dandiarchive.org/dandiset/000108/draft/files?location={sub}%2Fses-{ses}%2Fmicr%2F">{ses}</a>'
         photo_url = get_photo_url(photos, sample)
         if photo_url:
-            row += f""" <btn class="button" onClick="showMyImage('{photo_url}');">Photo</btn>"""
+            row += f"""  <btn class="button" onClick="showMyImage('{photo_url}');">Photo</btn>"""
         row += '<br/>\n'
         html += row
+    if noprocess:
+        html += "Could not process:"
+        for val in noprocess:
+            html += f" {val}"
+        html += "<br/>\n"
     html += "</body></html>\n"
 
-    with open("index.html", "wt") as fp:
+    with open(f"index-{sub}.html", "wt") as fp:
         fp.write(html)
+    print('could not process:', noprocess)
